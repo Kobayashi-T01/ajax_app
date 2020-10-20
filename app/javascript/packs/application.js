1de3148995f4cc7e80bee7ab81c5fdebef18440c
Kobayashi-T01 <t-kobayashi@MacBook-Pro.local>
2020-10-20 15:48:06 +0900

    既読機能実装フロント側

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../checked")
-require("../memo")
+require("/Users/t-kobayashi/projects/ajax_app/app/javascript/packs/checked")
+require("/Users/t-kobayashi/projects/ajax_app/app/javascript/packs/memo")
+// 自分のPCでパスを指定すること
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
